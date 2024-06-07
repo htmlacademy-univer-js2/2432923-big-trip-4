8cfeb9e3229abd5acb5043e34af9e484a5865bbf
Polina <polinakuzmina2904@gmail.com>
2024-06-07 23:24:07 +0500

    убрала лишнее в работе фильтра

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,4 +1,3 @@
-import { FilterType } from '../consts.js';
 import AbstractView from '../framework/view/abstract-view.js';
 import { createFilterTemplate } from '../templates/filter-template.js';
 
@@ -14,25 +13,14 @@ export default class FilterView extends AbstractView {
     this.#currentFilter = currentFilter;
 
     this.element.addEventListener('change', this.#itemChangeHandler);
-    this.#setFilterChangeHandler();
-
   }
 
   get template() {
     return createFilterTemplate({ filters: this.#items, currentFilter: this.#currentFilter });
   }
 
-  #setFilterChangeHandler() {
-    for (const filterName of Object.values(FilterType)) {
-      const filterInput = this.element.querySelector(`#filter-${filterName.toLowerCase()}`);
-      filterInput.addEventListener('change', this.#itemChangeHandler);
-    }
-  }
-
   #itemChangeHandler = (evt) => {
     evt.preventDefault();
     this.#onItemChange?.(evt.target.value);
-    this.#currentFilter = evt.target.value;
-    return createFilterTemplate({ filters: this.#items, currentFilter: this.#currentFilter });
   };
 }
